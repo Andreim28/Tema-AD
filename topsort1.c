487a82a232124d7bace48ea0887ec53c20729fa5
Andreim28 <32871168+Andreim28@users.noreply.github.com>
2018-06-02 11:55:39 +0300

    Update topsort1.c

--- topsort1.c
+++ topsort1.c
@@ -7,38 +7,35 @@
 
 int Matrix[noVertexMAX][noVertexMAX];
 
-void topSort1(int Matrix[noVertexMAX][noVertexMAX]){
+int internal_degree(int Matrix[noVertexMAX][noVertexMAX],int vertex)
+{
+    int it1;
+    int sum=0;
+    for(it1=0;it1<noVertex;it1++){
+        sum=sum+Matrix[it1][vertex];
+    }
+    return sum;
+}
+void topSort(int Matrix[noVertexMAX][noVertexMAX])
+{
     int it1;
     int it2;
-    int it3;
-    int sum[noVertexMAX];
     int visited[noVertexMAX];
     int count=0;
 
-    for(it1=0;it1<noVertex;it1++){
-        sum[it1]=0;
-        visited[it1]=0;
-    }
-
     for(it1=0;it1<noVertex;it1++)
-        for(it2=0;it2<noVertex;it2++)
-            sum[it1]=sum[it1]+Matrix[it2][it1];
-
-    printf("\nThe topological order using method 1 is:");
-
-    while(count<noVertex){
-        for(it3=0;it3<noVertex;it3++){
-            if((sum[it3]==0) && (visited[it3]==0)){
-                printf("%d ",(it3));
-                visited[it3]=1;
-            }
-
-            for(it1=0;it1<noVertex;it1++){
-                if(Matrix[it1][it3]==1)
-                    sum[it3]--;
-            }
+        visited[it1]=0;
+    while(count<noVertex)
+    {
+        for(it1=0;it1<noVertex;it1++)
+            if(internal(Matrix,it1)==0&&visited[it1]==0)
+        {
+             printf("%d ",(it1));
+             visited[it1]=1;
+             for(it2=0;it2<noVertex;it2++)
+                Matrix[it1][it2]=0;
+             count++;
         }
-        count++;
     }
 }
 int main()
@@ -53,6 +50,6 @@ int main()
         for(it2=0;it2<noVertex;it2++)
             fscanf(randMatrix,"%d",&Matrix[it1][it2]);
     }
-    topSort1(Matrix);
+    topSort(Matrix);
     fclose(randMatrix);
 }
