79bb5ef49ee119d70a634f3ea691c6d004f9a590
Andreim28 <32871168+Andreim28@users.noreply.github.com>
2018-06-05 15:54:15 +0300

    Upload

--- Code/topSortInternal.c
+++ Code/topSortInternal.c
@@ -21,13 +21,12 @@ void topSort_internal(int Matrix[noVertexMAX][noVertexMAX])
     while(count<noVertex)
     {
         for(it1=0;it1<noVertex;it1++)
-            if(internal_degree(Matrix,it1)==0&&visited[it1]==0)
-        {
-             printf("%d ",(it1));
-             visited[it1]=1;
-             for(it2=0;it2<noVertex;it2++)
-                Matrix[it1][it2]=0;
-             count++;
-        }
+            if(internal_degree(Matrix,it1)==0&&visited[it1]==0){
+                printf("%d ",(it1));
+                visited[it1]=1;
+                for(it2=0;it2<noVertex;it2++)
+                    Matrix[it1][it2]=0;
+                count++;
+            }
     }
 }
